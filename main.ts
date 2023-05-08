8661028d19ee0de45efd63d3e826269f5cc373ae
Egyed József <jegyed50@gmail.com>
2023-05-08 09:16:56 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onSound(DetectedSound.Quiet, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.forever(function () {
+	
+})
